f8ed9b4fc8850988baef34c1fe3e6abc6c742b4e
Henry Niermann <55467018+hniermann@users.noreply.github.com>
2022-11-27 13:39:05 -0500

    Add files via upload

---
+++ VideoEditingCode.m
@@ -0,0 +1,15 @@
+% Read in video 1
+vid1 = VideoReader('OG1.MOV');
+vid1.CurrentTime = 9;
+currAxes = axes;
+num = 1;
+% Iterate and look for end. Then get 10 frames between 1 and num
+while hasFrame(vid1)
+    vidFrame = readFrame(vid1);
+    image(vidFrame, 'Parent', currAxes);
+    currAxes.Visible = 'off';
+    pause(1/vid1.FrameRate);
+    display(num);
+    pause;
+    num = num + 1;
+end
